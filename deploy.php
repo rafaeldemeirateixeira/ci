27e98c3590f020d13726cb83de8030c504ba8096
Rafael Teixeira <rafaeldemeirateixeira@gmail.com>
2020-01-29 16:12:26 -0300

    Adicionado pacotes para CI

--- deploy.php
+++ deploy.php
@@ -42,7 +42,7 @@ task('vendors', function () {
     if (!commandExist('unzip')) {
         writeln('<comment>To speed up composer installation setup "unzip" command with PHP zip extension https://goo.gl/sxzFcD</comment>');
     }
-
+    run('cd {{release_path}} && {{bin/composer}} {{composer_options}}');
     if (input()->hasOption('vendors')) {
         $vendors = input()->getOption('vendors');
         if ($vendors == 'install') {
